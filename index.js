e2bbc4446c18a2aed5f10cf1f89eed4790621a65
Eduardo Silva <edupsilva4330@gmail.com>
2024-09-10 22:55:25 -0300

    iniciando curso

---
+++ index.js
@@ -0,0 +1,15 @@
+// arrays, objetos
+let meta = {
+  value: "ler um livro por mês",
+  checked: false,
+  log: (info) => {
+    console.log(info);
+  },
+};
+
+meta.log(meta.value);
+
+// function // arrow function
+const criarMeta = () => {};
+
+// function criarMeta () {}
